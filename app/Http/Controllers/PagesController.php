4fa756d771fe51cc1cddd1fe616f26f8d2d8ac3b
Kirilov,Martin M.K <martin.kirilov@student.fontys.nl>
2020-05-17 22:00:30 +0300

    week 1

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+      $title = 'Welcome to our pet blog!';
+      return view('pages.index')->with('title',$title);
+    }
+
+    public function about(){
+      $title = 'About us';
+      return view('pages.about')->with('title',$title);
+    }
+
+    public function services(){
+      $data = array(
+        'title' => 'Services',
+        'services' => ['Dogs','Cats','Fish','Birds']
+      );
+      return view('pages.services')->with($data);
+    }
+}
